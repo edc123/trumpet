3538ceacf7016805684cb7cad7c99ac6150ff0f7
Ed Cheng <edc.123@hotmail.com>
2016-08-21 14:17:10 +0800

    OMG IT FUCKING WORKS GENERATORS ARE AMAZING

--- app/data/index.js
+++ app/data/index.js
@@ -1,59 +1,28 @@
 'use strict'
 
-const config = require('../../etc')
-const moment = require('moment')
-const NYT    = require('nyt')
-const nyt    = new NYT(config.articleSearchKey)
-const util   = require('../util')
+const moment     = require('moment')
+const Promise    = require('bluebird')
+const util       = require('../util')
 
-let initialDate = '19760101'
+let initialDate  = '20160101'
+let year         = moment(initialDate).format('YYYY')
 
-// I envision a promise.all eventually here...for all 91 promises...
+Promise.coroutine(function* () {
+	
+	let headlines    = []
+	let initialSearch = yield util.nytSearch(initialDate, 0)
+	let meta = yield util.readMeta(initialSearch, year)
+	let pagesRemaining = Math.floor((meta.hits - meta.offset)/10)
+	let initialResult = yield util.processHeadlines(initialSearch)
+	Array.prototype.push.apply(headlines, initialResult)
 
-nytHalfYear(initialDate)
-	.then((results) => {
-		util.writeToJSON(results)
-	})
+	for(let i = 1; i <= pagesRemaining; i++) {
+		let result = yield util.nytSearch(initialDate, i)
+		let resultProcessed = yield util.processHeadlines(result)
+		Array.prototype.push.apply(headlines, resultProcessed)
+	}
 
-// do two of the following promise to get 1 years data...eventually do a lot of these? but the api...
+	// console.log(headlines, meta)
+	yield util.writeToTxt(headlines, year)
 
-function nytHalfYear(date) {
-	// This needs to be a promise
-	return new Promise((resolve, reject) => {
-		let dateAndAHalf = moment(date).add(6, 'months').format('YYYYMMDD')
-		nyt.article.search({
-			'fq': 'headline:\"Trump\" AND persons:\"Trump, Donald J\"',
-			'fl': 'headline,pub_date,web_url',
-			begin_date: date,
-			end_date: dateAndAHalf,
-			sort: 'oldest',
-			page: '0' // needs to be incrementable with a while loop
-		}, (results) => {
-			if (JSON.parse(results).status === 'ERROR') reject(console.log('Error with nyt api!'))
-			else {
-				// while response.meta.hits > 10, recur nytHalfYear but increment page++
-				// then when done -- resolve(results) // need to paginate IF available ie do another nythalfyear...maybe a while loop??, else resolve the results object
-			}
-		})
-	})
-}
-// once off scrape...
-//
-// LOOP (stop at 20160701 (91 iter)) DO nytHalfYear, nest loop over ceil(response.meta.hits / 10)
-// times to do pages......every two iter, UTIL.writeFile to "thatYear".json, if anything returns...
-//
-// UTIL FUNC - FILTER TRUMPLINES - on "thatYear.json", filter out to just the *main* headline and pub_date ... highland streaming map reduce magicks here
-//
-// UTIL FUNC - COUNTER - log to meta.json under the year counter the response meta items each half year
-// to corresponding year.
-//
-// UTIL FUNC - ARBITRARY TRUMPLINE UPDATE (date 1, date 2) - do start date as 20160701 / 20160702 - then
-// end date 20160814 (92nd final iter).
-//
-// UTIL FUNC - SENTIMENT ANALYSIS - sentiment analysis on everything producing new file in final folder,
-// COUNTER to add count of positive and negative per year??
-//
-// every user visit...
-//
-// do new arbitrary update, if new, filter from memory, sentiment and push to 2016.json
-// deal with the consequences on client side js.........progress bar while the update happens iff?
+})()
--- app/util/index.js
+++ app/util/index.js
@@ -1,7 +1,64 @@
 'use strict'
 
-const fs = require('fs')
+const config      = require('../../etc')
+const fs        = require('fs')
+const moment       = require('moment')
+const NYT          = require('nyt')
+const nyt          = new NYT(config.articleSearchKey)
 const sentiment = require('sentiment')
+
+function nytSearch(date, pageNumber) {
+	return new Promise((resolve, reject) => {
+		nyt.article.search({
+			'fq':       'headline:\"Trump\" AND persons:\"Trump, Donald J\"',
+			'fl':       'headline,pub_date,web_url',
+			begin_date: date,
+			end_date:   moment(date).add(12, 'months').format('YYYYMMDD'),
+			sort:       'oldest',
+			page:       pageNumber
+		}, (results) => {
+			let metaJSON = JSON.parse(results).response
+			if (metaJSON.status === 'ERROR') console.log('Error with nyt api!')
+			else resolve(results)
+		})
+	})
+}
+
+function writeToTxt(arg, filename) {
+	return new Promise((resolve, reject) => {
+		let path = './app/data/' + filename + '.txt'
+		console.log('no errors so writing now to ', path)
+		fs.appendFile(path, JSON.stringify(arg), 'utf8', (err) => {
+			if(err) throw err
+			console.log('Done! ', path)
+		})
+		resolve(1)
+	})
+}
+
+function readMeta(data, year) {
+	return new Promise((resolve, reject) => {
+		let rawJSON = JSON.parse(data).response.meta
+		let meta = {
+			year,
+			hits: rawJSON.hits,
+			offset: rawJSON.offset
+		}
+		resolve(meta)
+	})
+}
+
+function processHeadlines(data) {
+	return new Promise((resolve, reject) => {
+		let headlinesJSON = JSON.parse(data).response.docs
+		let processed = headlinesJSON.map(doc => ({
+			headline: doc.headline.main,
+			pub_date: moment(doc.pub_date).format('YYYYMMDD'),
+			web_url: doc.web_url
+		}))
+		resolve(processed)
+	})
+}
 
 // need a function to seperate meta from docs array
 // then call write to JSON on each...
@@ -9,18 +66,23 @@ const sentiment = require('sentiment')
 	//write meta to totals.json
 	//write docs to year.json e.g. '1974.json'
 
+		// let meta = rawJSON.meta
+		// let docs = rawJSON.docs
+		// console.log(meta)
+		// console.log(docs)
 
-function writeToJSON(arg) {
-	//this should write a given array / object to file
-	console.log('no errors so writing now!')
-}
+		// return 
 
-
+		// while response.meta.hits > 10, recur nytHalfYear but increment pageNumber++ ... so here we check if we need to continue in this date space
+		// then when done -- resolve(results) // need to paginate IF available ie do another nythalfyear...maybe a while loop??, else resolve the results object
 
 //ARBITRARY TRUMPLINE UPDATE
 
 // SENTIMENT ANALYSIS
 
 module.exports = {
-	writeToJSON
+	nytSearch,
+	writeToTxt,
+	readMeta,
+	processHeadlines
 }
